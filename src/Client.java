e77e1aa65385ae1fc430675287690046695bf1d2
Roberto Rodriguez <brod6074@users.noreply.github.com>
2015-01-27 10:14:07 -0800

    Can scan simple words into symbol table

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,6 @@
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.PushbackReader;
 
 
 public class Client {
@@ -7,16 +8,18 @@ public class Client {
 	public static void main(String[] args) throws Exception {
 		// open up a file stream for the Toy source code and pass to
 		// lexical analyzer
-		String filename = "input1.txt";
-		BufferedReader in = new BufferedReader(new FileReader(filename));
-		ToyLexicalAnalyzer scanner = new ToyLexicalAnalyzer(in);
+		String filename = "words1.txt";
+		//BufferedReader in = new BufferedReader(new FileReader(filename));
+		PushbackReader source = new PushbackReader(new FileReader(filename));
+		ToyLexicalAnalyzer toyLexer = new ToyLexicalAnalyzer(source);
+		toyLexer.scanNextToken();
+		toyLexer.scanNextToken();
+		toyLexer.scanNextToken();
+		toyLexer.scanNextToken();
+		toyLexer.scanNextToken();
 		
-		// print out the tokens of the source file
-		// for (iter = scanner.getTokenIterator(); iter != null; iter++)
-		//		System.out.print(iter.getTokenString);
-		
-		// print out contents of trie table
-		// code to print table
+		//toyLexer.dumpTokens() prints tokens to console
+		//toyLexer.dumpSymTab() prints the symbol table to the console
 		
 	}
 
--- src/ToyLexicalAnalyzer.java
+++ src/ToyLexicalAnalyzer.java
@@ -1,5 +1,7 @@
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PushbackReader;
+//import java.util.Collection;
 
 /**
  * 
@@ -7,51 +9,60 @@ import java.io.IOException;
  *
  */
 public class ToyLexicalAnalyzer {
+	//private static final char EOF = (char) -1;
 	
-	private SourceStream SourceStream;
+	private PushbackReader source;
+	private Trie symTab;
+	//private Collection<ToyToken> tokens;
 	
-	/**
-	 * TODO
-	 */
-	public ToyLexicalAnalyzer(BufferedReader in) throws IOException {
-		
+	public ToyLexicalAnalyzer(PushbackReader source) {
+		this.source = source;
+		symTab = new Trie();
 	}
 	
 	
 	/**
-	 * TODO
+	 * Scans the input stream and constructs the next recognized token.
+	 * 
+	 * @return int representing the token class 
 	 */
-	public int getNextToken() {
+	public int scanNextToken() throws IOException {
+		char currentChar, peekChar;
+		currentChar = (char)source.read();
 		
+		// skip whitespace
+		while (isWhiteSpace(currentChar))
+			currentChar = (char)source.read();
+		
+		// code for handling identifiers/keywords/booleans
+		if (Character.isLetter(currentChar)) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(currentChar);
+			peekChar = (char)source.read();
+			
+			while (Character.isLetterOrDigit(peekChar) || peekChar == '_') {
+				sb.append(peekChar);
+				peekChar = (char)source.read();
+			}
+			
+			String s = sb.toString();
+			
+			// determine if token should be id, keyword, or boolean
+			
+			// if id, add to symbol table
+			symTab.insert(s);
+		}
+		if (Character.isDigit(currentChar)) {
+			// code to handle integers (decimal or hex)
+			// code to handle floating point
+		}
 		
 		return -1;
 	}
 	
-	
-	
-	/**
-	 * 
-	 * SourceStream
-	 *
-	 */
-	private static class SourceStream {
-		private BufferedReader in;
-		
-		private SourceStream(BufferedReader in) {
-			this.in = in;
-		}
-		
-		private void ignoreWhitespace() {
-			
-		}
-		
-		public char getNextChar() throws IOException {
-			return (char)in.read();
-		}
-		
-		
-	}
-	
+	private boolean isWhiteSpace(char c) {
+		return (c == ' ') || (c == '\t') || (c == '\n') || (c == '\r');
+	}	
 	
 	/**
 	 * 
@@ -61,13 +72,79 @@ public class ToyLexicalAnalyzer {
 	private static class Trie {
 		private static final int MAX_TRANSITION = 63;
 		private static final int ALPHABETIC_CHARS = 52;
+		private static final int EMPTY = -1;
+		private static final char KEYWORD_TERMINAL = '!';
+		private static final char ID_TERMINAL = '@';
 		
-		private int[] 	trieSwitch = new int [ALPHABETIC_CHARS];
-		private char[] 	trieSymbol = new char [MAX_TRANSITION];
-		private int[] 	trieNext = new int[MAX_TRANSITION];
+		private int 	nextFreeSpot;
+		private int[] 	trieSwitch;
+		private char[] 	trieSymbol;
+		private int[] 	trieNext;
+		
+		public Trie() {
+			nextFreeSpot = 0;
+			
+			trieSwitch = new int [ALPHABETIC_CHARS];
+			for (int i = 0; i < trieSwitch.length; i++)
+				trieSwitch[i] = EMPTY;
+			
+			trieSymbol = new char [MAX_TRANSITION];
+			trieNext = new int[MAX_TRANSITION];
+			for (int i = 0; i < trieSymbol.length; i++) {
+				trieSymbol[i] = ' ';
+				trieNext[i] = EMPTY;
+			}
+		}
+		
+		void insert(String s) {
+			// use first character to determine index in switch array
+			char c = s.charAt(0);
+			int switchIndex = getSwitchIndex(c);
+			
+			if (trieSwitch[switchIndex] == EMPTY)
+				trieSwitch[switchIndex] = nextFreeSpot;
+			
+			int currPos = trieSwitch[switchIndex];
+			for (int i = 1; i < s.length(); i++) {
+				c = s.charAt(i);
+				if (trieSymbol[currPos] == ' ' || trieSymbol[currPos] == c) {
+					trieSymbol[currPos++] = c;
+				} else {
+					if (trieNext[currPos] == EMPTY) {
+						trieNext[currPos] = nextFreeSpot;
+						currPos = nextFreeSpot;
+						trieSymbol[currPos++] = c;
+					} else {
+						//
+					}
+				}
+			}
+			
+			// if keyword, trieSymbol[position++] = KEYWORD_TERMINAL;
+			// else , trieSymbol[position++] = ID_TERMINAL;
+			// nextFreeSpot = position;
+			trieSymbol[currPos++] = '@';
+			nextFreeSpot = currPos;
+			
+		}
+		
+		boolean isReserved(String s) {
+			return true;
+		}
+		
+		/**
+		 * Returns an appropriate number (between 0 and ALPHABETIC_CHARS - 1) 
+		 * for indexing into the switch array depending on the char parameter.
+		 *  
+		 * @param c
+		 * @return
+		 */
+		private int getSwitchIndex(char c) {
+			return ((int) c) - 65;
+		}
 	}
 	
-	private enum ToyToken {
+	/*private enum ToyToken {
 		_boolean(1, "boolean"),
 		_break(2, "break"),
 		_class(3, "class"),
@@ -98,47 +175,42 @@ public class ToyLexicalAnalyzer {
 		_equal(28, "equal"),
 		_notequal(29, "notequal"),
 		_and(30, "and"),
-		_or(31), 
-		_not(32), 
-		_assignop(33), 
-		_semicolon(34), 
-		_comma(35),
-		_period(36), 
-		_leftparen(37, "leftparen"), 
-		_rightparen(38, "rightparen"), 
-		_leftbracket(39),
-		_rightbracket(40), 
-		_leftbrace(41), _rightbrace(42), 
+		_or(31, "or"),
+		_not(32, "not"),
+		_assignop(33, "assignop"),
+		_semicolon(34, "semicolon"),
+		_comma(35, "comma"),
+		_period(36, "period"),
+		_leftparen(37, "leftparen"),
+		_rightparen(38, "rightparen"),
+		_leftbracket(39, "leftbracket"),
+		_rightbracket(40, "rightbracket"),
+		_leftbrace(41, "leftbrace"),
+		_rightbrace(42, "rightbrace"),
 		_intconstant(43, "intconstant"),
-		_doubleconstant(44), 
-		_stringconstant(45), 
-		_booleanconstant(46), 
-		_id(47),
+		_doubleconstant(44, "doubleconstant"),
+		_stringconstant(45, "stringconstant"),
+		_booleanconstant(46, "booleanconstant"),
+		_id(47, "id"),
 		// special tokens for recognizing/ignoring whitespace
 		_space(-1, "space"),
 		_tab(-1, "tab"),
-		_newline(-2, "newline");
+		_newline(-2, "newline"),
+		_eof(-3, "eof");
 		
 		private final int tokenNum;
 		private final String tokenString;
-		//private boolean bKeyword;
 		
 		ToyToken(int num) { 
 			this(num, null); 
-			//bKeyword = false;
 		}
 		
 		ToyToken(int num, String keyword) { 
 			tokenNum = num; tokenString = keyword;
-			//bKeyword = true;
 		}
 		
 		public int getTokenNumber() { return tokenNum; }
 		
-		public boolean isKeyword() { return bKeyword; }
-		
-	}
-	
-	
+	}*/
 
-}
+} // end of class ToyLexicalAnalyzer
